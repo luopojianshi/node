cc587208530d7e859f3681631f50aef93ca23d76
lijiafu <lijiafu@backustech.com>
2017-05-27 15:44:57 +0800

    express 练习, 没懂

---
+++ express/test.js
@@ -0,0 +1,14 @@
+// test.js 文件
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('hi, good luck!');
+});
+
+var server = app.listen(8081, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('应用实例, 访问地址: http://%s:%s', host, port);
+});
